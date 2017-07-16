5884342f1e245eaf6c431721abf747cfbbb6e21c
Matt Gregory <mgregory22@gmail.com>
2017-07-16 18:50:21 -0400

    Add displaying events and posting an event to firebase

--- src/App.js
+++ src/App.js
@@ -1,81 +1,39 @@
 import React, { Component } from 'react';
 import { Button } from 'react-bootstrap';
 import './App.css';
-import { EventList } from './Event'
+import { EventList } from './Event';
+import PostAnEvent from './PostAnEvent';
+import firebase from './firebase';
 
 class App extends Component {
   constructor(props) {
     super(props);
 
-    // dummy data
     this.state = {
-      events: [
-        {
-          what: "DJ Motaz",
-          where: "West Bay Beach Resort",
-          when: new Date("2017-08-10 22:00:00"),
-          desc: "DJ Motaz Live at our on-site View Nightclub from 10 pm - 2 am at West Bay Beach",
-          tags: ["music", "art", "alcohol"]
-        },
-        {
-          what: "Monday Night Trivia!",
-          where: "The Workshop",
-          when: new Date("2017-08-06 13:00:00"),
-          desc: "A night of trivia.",
-          tags: ["trivia", "alcohol"]
-        },
-        {
-          what: "Monte Nagler: Visions of Light",
-          where: "Crooked Tree Arts Center",
-          when: new Date("2017-08-05 16:30:00"),
-          desc: "A collection of photographic work by Michigan’s own Monte Nagler, a former student of Ansel Adams.",
-          tags: ["art"]
-        },
-        {
-          what: "DJ Motaz",
-          where: "West Bay Beach Resort",
-          when: new Date("2017-08-10 22:00:00"),
-          desc: "DJ Motaz Live at our on-site View Nightclub from 10 pm - 2 am at West Bay Beach",
-          tags: ["music", "art", "alcohol"]
-        },
-        {
-          what: "Monday Night Trivia!",
-          where: "The Workshop",
-          when: new Date("2017-08-06 13:00:00"),
-          desc: "A night of trivia.",
-          tags: ["trivia", "alcohol"]
-        },
-        {
-          what: "Monte Nagler: Visions of Light",
-          where: "Crooked Tree Arts Center",
-          when: new Date("2017-08-05 16:30:00"),
-          desc: "A collection of photographic work by Michigan’s own Monte Nagler, a former student of Ansel Adams.",
-          tags: ["art"]
-        },
-        {
-          what: "DJ Motaz",
-          where: "West Bay Beach Resort",
-          when: new Date("2017-08-10 22:00:00"),
-          desc: "DJ Motaz Live at our on-site View Nightclub from 10 pm - 2 am at West Bay Beach",
-          tags: ["music", "art", "alcohol"]
-        },
-        {
-          what: "Monday Night Trivia!",
-          where: "The Workshop",
-          when: new Date("2017-08-06 13:00:00"),
-          desc: "A night of trivia.",
-          tags: ["trivia", "alcohol"]
-        },
-        {
-          what: "Monte Nagler: Visions of Light",
-          where: "Crooked Tree Arts Center",
-          when: new Date("2017-08-05 16:30:00"),
-          desc: "A collection of photographic work by Michigan’s own Monte Nagler, a former student of Ansel Adams.",
-          tags: ["art"]
-        },
-      ]
+      events: [],
     }
   }
+
+  componentDidMount() {
+    const eventsRef = firebase.database().ref('events');
+    eventsRef.on('value', (snapshot) => {
+      let events = snapshot.val();
+      let newEvents = [];
+      for (let event in events) {
+        newEvents.push({
+          id: event,
+          what: events[event].what,
+          when: events[event].when,
+          where: events[event].where,
+          desc: events[event].desc,
+        });
+      }
+      this.setState({
+        events: newEvents
+      });
+    });
+  }
+
   render() {
     return (
       <div className="App container">
@@ -100,6 +58,10 @@ class App extends Component {
          <div className="row events__main">
            <EventList events={this.state.events} />
          </div> 
+
+        <div className="row events__actions">
+          <PostAnEvent />
+        </div>
 
         <footer className="footer">
           <p>&copy; Cyberdelia 2017</p>
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase';
+
+const config = {
+  apiKey: "AIzaSyABljxdX4JeDbr5NGoeMyoAuUg1g9L4Dww",
+  authDomain: "tc-events.firebaseapp.com",
+  databaseURL: "https://tc-events.firebaseio.com",
+  projectId: "tc-events",
+  storageBucket: "tc-events.appspot.com",
+  messagingSenderId: "202682525327"
+};
+firebase.initializeApp(config);
+export default firebase;
