3f9b98c30c8664582d5ea6d66f5e24701d02c48b
Jared Miller <jtmiller@gmail.com>
2017-07-19 22:03:39 -0400

    move all firebase settings into a private file

--- src/data/firebase.js
+++ src/data/firebase.js
@@ -1,14 +1,5 @@
 import firebase from 'firebase';
-import { apiKey } from '../private/firebase';
-
-const config = {
-  apiKey: apiKey,
-  authDomain: "tc-events.firebaseapp.com",
-  databaseURL: "https://tc-events.firebaseio.com",
-  projectId: "tc-events",
-  storageBucket: "tc-events.appspot.com",
-  messagingSenderId: "202682525327"
-};
+import { config } from '../private/firebase';
 
 firebase.initializeApp(config);
 
